2b47f1623bda12f2b9de948fb8f4c60bac0574bf
wward <woward00@gmail.com>
2021-09-24 12:32:56 -0600

    Practical 9.1 complete

--- Practical_9.1_DigitalIO_Ward/Practical9.1main.asm
+++ Practical_9.1_DigitalIO_Ward/Practical9.1main.asm
@@ -26,7 +26,27 @@ StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
 ;-------------------------------------------------------------------------------
 ; Main loop here
 ;-------------------------------------------------------------------------------
+init:
+			mov.w	#000h, R5
+			mov.w	#000h, R6
 
+			bis.b	#BIT3, &P5DIR			; set P1.0 as output - led
+			bic.b	#BIT3, &P5OUT			; set init val of led to 0
+
+			bic.b	#BIT7, &P3DIR			; set P4.1 as in - jumper
+			bis.b	#BIT7, &P3REN			; EN pull up/down
+			bic.b	#BIT7, &P3OUT			; pull-down res.
+
+			bic.b	#LOCKLPM5, &PM5CTL0		; disable DIO low-power default
+
+main:
+			bis.b	#BIT3, &P5OUT			; enable led
+			mov.w	P3IN, R5
+			bic.b	#BIT3, &P5OUT			; disable led
+			mov.w	P3IN, R6
+
+			jmp		main
+			nop
                                             
 
 ;-------------------------------------------------------------------------------
