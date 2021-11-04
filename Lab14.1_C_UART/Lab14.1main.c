d8a3dba4be3d0de8292881e30825b6c6d61c4940
wward <woward00@gmail.com>
2021-11-03 23:28:41 -0600

    Lab14.1 and Lab14.1.2
    Sending First and Last with SW1 and SW2 to CCS terminal via Serial/UART working

--- Lab14.1_C_UART/Lab14.1main.c
+++ Lab14.1_C_UART/Lab14.1main.c
@@ -4,6 +4,12 @@
  *  11/1/2021
  *  UART
  */
+
+char message[] = "Walker Ward ";
+unsigned const int last = 7;
+unsigned int position;
+unsigned int message_length;
+
 int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
@@ -23,10 +29,22 @@ int main(void)
     UCA1MCTLW |= 0xD600;
 
     //--3. Config Ports
+    P1DIR |= BIT0;              // Config P1.0 LED1 (Red) as out
+    P1OUT &= ~BIT0;             // Init val = 0
+
+
+    P6DIR |= BIT6;              // Config P6.6 LED2 (Green) as out
+    P6OUT &= ~BIT6;             // Init val = 0
+
     P4DIR &= ~BIT1;             // Clear P4.1 (SW1) dir = in
     P4REN |= BIT1;              // Enable pull up/down res
     P4OUT |= BIT1;              // Make res pull up
-    P4IES |= BIT1;              // Config IRQ H->L
+    P4IES &= ~BIT1;              // Config IRQ H->L
+
+    P2DIR &= ~BIT3;             // Clear P2.3 (SW2) dir = in
+    P2REN |= BIT3;              // Enable pull up/down res
+    P2OUT |= BIT3;              // Make res pull up
+    P2IES &= ~BIT3;              // Config IRQ H->L
 
     //--3.1. Tx
     P4SEL1 &= ~BIT3;
@@ -40,6 +58,9 @@ int main(void)
     // IRQs
     P4IFG &= ~BIT1;             // Clear P4.1 IRQ flag
     P4IE |= BIT1;               // Enable P4.1 IRQ
+
+    P2IFG &= ~BIT3;             // Clear P2.3 IRQ flag
+    P2IE |= BIT3;               // Enable P2.3 IRQ
 
     __enable_interrupt();       // EN maskable IRQ
 
@@ -57,24 +78,44 @@ int main(void)
 //-- END main
 
 //-- Interrupt Service Routines -------------------------
+// Service SW1
 #pragma vector = PORT4_VECTOR
-__interrupt void ISR_Port4_S1(void) {
+__interrupt void ISR_Port4_SW1(void) {
     position = 0;
     UCA1IE |= UCTXCPTIE;
     UCA1IFG &= ~UCTXCPTIFG;
     UCA1TXBUF = message[position];
 
+    P1OUT |= BIT0;
+    P6OUT &= ~BIT6;
     P4IFG &= ~BIT1;
 }
-//
+//-- End ISR_Port4_SW1
+
+// Service SW2
+#pragma vector = PORT2_VECTOR
+__interrupt void ISR_Port2_SW2(void) {
+    position = last;
+    UCA1IE |= UCTXCPTIE;
+    UCA1IFG &= ~UCTXCPTIFG;
+    UCA1TXBUF = message[position];
+
+    P1OUT &= ~BIT0;
+    P6OUT |= BIT6;
+    P2IFG &= ~BIT3;
+}
+//-- End ISR_Port2_SW2
 
 #pragma vector = EUSCI_A1_VECTOR
 __interrupt void ISR_EUSCI_AL(void) {
-    if(position == sizeof(message)) {
+    if(position+1 == sizeof(message) || position == 6) { // || position == (last - 1)
         UCA1IE &= ~UCTXCPTIE;
+        int i;
+        for(i=0; i<10000; i++){}
     } else {
         position++;
         UCA1TXBUF = message[position];
     }
     UCA1IFG &= ~UCTXCPTIFG;
 }
+//-- End ISR_EUSCI_AL
